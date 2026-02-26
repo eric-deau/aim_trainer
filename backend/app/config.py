c99fbaa538a6eb450b606c9eb13a8ee9c2f00bda
eric-deau <edeau@my.bcit.ca>
2026-02-26 14:45:55 -0800

    Added production config

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,21 +4,19 @@ from datetime import timedelta
 class Config:
     
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "mysql+pymysql://user:pass@127.0.0.1:3306/aim_trainer",
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_TYPE = os.environ.get("SESSION_TYPE", "filesystem")
     SESSION_PERMANENT = False
     
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SAMESITE = os.environ.get("SESSION_COOKIE_SAMESITE", "Lax")
+
 
     PERMANENT_SESSION_LIFETIME = timedelta(days=30)
 
-    #SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", "false").lower() == "true"
+    SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", "false").lower() == "true"
 
 class DevelopmentConfig(Config):
     DEBUG = True
